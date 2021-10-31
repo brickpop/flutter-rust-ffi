08b905c93555715186d05a148e6e5860390f64df
Oleg Okhotnikov <olegnet@gmail.com>
2021-11-01 00:58:41 +0300

    Macos integration (#27)
    * Added macos integration example: part 1
    * Added macos integration example: part 2
    * Added macos integration example: part 3

--- lib/mylib.dart
+++ lib/mylib.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:ffi/ffi.dart';
 import './bindings.dart';
 
-const DYNAMIC_LIBRARY_FILE_NAME = "libexample.so";
+const DYNAMIC_LIBRARY_FILE_NAME_ANDROID = "libexample.so";
+const DYNAMIC_LIBRARY_FILE_NAME_MACOS = "libexample.dylib";
 
 /// Wraps the native functions and converts specific data types in order to
 /// handle C strings.
@@ -14,19 +15,21 @@ class Greeter {
 
   static DynamicLibrary _loadLibrary() {
     return Platform.isAndroid
-        ? DynamicLibrary.open(DYNAMIC_LIBRARY_FILE_NAME)
-        : DynamicLibrary.process();
+        ? DynamicLibrary.open(DYNAMIC_LIBRARY_FILE_NAME_ANDROID)
+        : Platform.isMacOS
+            ? DynamicLibrary.open(DYNAMIC_LIBRARY_FILE_NAME_MACOS)
+            : DynamicLibrary.process();
   }
 
   /// Computes a greeting for the given name using the native function
   static String greet(String name) {
-    final ptrName = Utf8.toUtf8(name).cast<Int8>();
+    final ptrName = name.toNativeUtf8().cast<Int8>();
 
     // Native call
     final ptrResult = _bindings.rust_greeting(ptrName);
 
     // Cast the result pointer to a Dart string
-    final result = Utf8.fromUtf8(ptrResult.cast<Utf8>());
+    final result = ptrResult.cast<Utf8>().toDartString();
 
     // Clone the given result, so that the original string can be freed
     final resultCopy = "" + result;
@@ -39,7 +42,7 @@ class Greeter {
 
   /// Releases the memory allocated to handle the given (result) value
   static void _free(String value) {
-    final ptr = Utf8.toUtf8(value).cast<Int8>();
+    final ptr = value.toNativeUtf8().cast<Int8>();
     return _bindings.rust_cstr_free(ptr);
   }
 }
---
+++ macos/README.md
@@ -0,0 +1,17 @@
+
+1. Run `flutter create --platforms=macos .`
+2. Remove unnecessary new files created in step 2
+3. Add some lines from `ios/mylib.podspec` to `macos/mylib.podspec`
+4. Replace `macos/Classes/*` with `ios/Classes/*`
+5. Few more changes in source code to compatibility with new ffi and ffigen
+
+If you start `flutter run` at this point you will get something like that
+```
+══╡ EXCEPTION CAUGHT BY WIDGETS LIBRARY ╞═══════════════════════════════════════════════════════════
+The following ArgumentError was thrown attaching to the render tree:
+Invalid argument(s): Failed to lookup symbol (dlsym(RTLD_DEFAULT, rust_greeting): symbol not found)
+```
+
+You can try changing `example/macos/Podfile` to resolve this error, but there is 
+[one more recipe](https://flutter.dev/docs/development/platform-integration/c-interop#compiled-dynamic-library-macos)
+to do that.
